2a2683025aaa63209ba239f462ad546e36bbd781
Monishobaid <monishobaid@gmail.com>
2025-03-05 21:45:07 +0530

    auth-file

---
+++ src/services/api/axios.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3000';
+
+export const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
