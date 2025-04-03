3d4cd5f07424bae66d35caf50a6759abbffc9f53
Monishobaid <monishobaid@gmail.com>
2025-04-03 13:47:57 +0300

    changes

--- src/components/BreadCrumbs/index.tsx
+++ src/components/BreadCrumbs/index.tsx
@@ -1,6 +1,5 @@
 // dependencies
 import React from "react";
-import { NavLink } from "react-router";
 import useBreadcrumbs from "use-react-router-breadcrumbs";
 import { CaretRight } from "@phosphor-icons/react";
 
@@ -10,7 +9,7 @@ const Breadcrumbs: React.FC = () => {
   return (
     <div className="w-full flex items-center gap-x-2">
       {breadcrumbs.map(({ breadcrumb, key, location }, index) => (
-        <div key={index} className="flex items-center gap-x-2">
+        <div key={key} className="flex items-center gap-x-2">
           {index > 0 ? (
             <CaretRight
               className="text-breadcrumb-inactive"
@@ -19,16 +18,15 @@ const Breadcrumbs: React.FC = () => {
             />
           ) : null}
 
-          <NavLink
-            to={key}
-            className={`anchor ${
+          <span
+            className={`${
               key === location.pathname
                 ? "text-breadcrumb"
                 : "text-breadcrumb-inactive"
             }`}
           >
             {breadcrumb}
-          </NavLink>
+          </span>
         </div>
       ))}
     </div>
