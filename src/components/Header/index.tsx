2970c7f9f7adac7bb329593b3a0dcadf826d8be6
Saif Ullah <saif.ullah@fuelbuddy.in>
2025-07-10 03:44:14 +0530

    added logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { useNavigate } from "react-router";
 import { useTranslation } from "react-i18next";
 
+// assets
+import TSLLogo from "@/assets/TSLLogo.svg?react";
+
 // components
 import TSLogixLogo from "@/assets/TSLogixLogo.webp";
 import BreadCrumbs from "../BreadCrumbs";
@@ -10,16 +13,20 @@ import LanguageSwitcher from "../LanguageSwitcher";
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
-  const { t } = useTranslation(['common']);
+  const { t } = useTranslation(["common"]);
   return (
-    <div className="w-full flex bg-white items-center justify-between px-4">
+    <div className="relative w-full flex bg-white items-center justify-between px-4">
+      <div className="rounded-md absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <TSLLogo className="w-36" />
+      </div>
+
       <div className="flex items-center gap-x-4">
         <div>
-          <img 
-            onClick={() => navigate("/")} 
-            src={TSLogixLogo} 
-            className="w-64 cursor-pointer" 
-            alt={t('common:logo_alt_text')}
+          <img
+            onClick={() => navigate("/")}
+            src={TSLogixLogo}
+            className="w-64 cursor-pointer"
+            alt={t("common:logo_alt_text")}
           />
         </div>
         <div>
